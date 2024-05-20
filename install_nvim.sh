02859ec08f834d88ffbbfbd827a7edab40ce8793
Pepinelli <lpepinelli@gmail.com>
2024-05-20 09:22:17 -0300

    zsh and vscodevim config

---
+++ install_nvim.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get install fuse libfuse2 -y
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+sudo mv nvim.appimage /usr/local/bin/nvim
+
