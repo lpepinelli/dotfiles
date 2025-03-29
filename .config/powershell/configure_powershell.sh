3e8b9ca2f428759b042d4fcd1c44b382de448eed
Pepinelli <43144939+lpepinelli@users.noreply.github.com>
2025-03-29 11:48:33 -0300

    Add files via upload

---
+++ .config/powershell/configure_powershell.sh
@@ -0,0 +1,14 @@
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+scoop install curl jq nvm
+winget install --id Git.Git -e --source winget
+scoop install neovim gcc
+echo ". $env:USERPROFILE\.config\powershell\user_profile.ps1" > $PROFILE.CurrentUserCurrentHost
+Install-Module posh-git -Scope CurrentUser -Force
+winget install JanDeDobbeleer.OhMyPosh -s winget
+Install-Module -Name Terminal-Icons -Repository PSGallery -Force
+Install-Module -Name z -Force
+Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
+scoop install fzf
+Install-Module -Name PSFzf -Scope CurrentUser -Force
