f9e00b6e908be3510907ad6601e50b46d8f0c716
Pepinelli <43144939+lpepinelli@users.noreply.github.com>
2025-03-29 11:50:58 -0300

    Update user_profile.ps1

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -1,11 +1,17 @@
+# Alias
+Set-Alias vim nvim
+Set-Alias grep findstr
+Set-Alias cat bat
+
 # set PowerShell to UTF-8
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
 Import-Module posh-git
-Import-Module oh-my-posh
+#Import-Module oh-my-posh ## Not supported anymore
 $omp_config = Join-Path $PSScriptRoot ".\pepinelli.omp.json"
 oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
 
+#Icons
 Import-Module -Name Terminal-Icons
 
 # PSReadLine
@@ -15,8 +21,13 @@ Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
 # Env
 $env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
+ssh-agent
 
 # Utilities
 function which ($command) {
